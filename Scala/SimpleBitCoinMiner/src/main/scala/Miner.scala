7639c01d74a2a91e2dafac904d74167d356bfa38
RayZ_O <rayzhang1011@gmail.com>
2015-09-18 22:42:23 -0400

    Removed unused import and misleading variable name

--- Scala/SimpleBitCoinMiner/src/main/scala/Miner.scala
+++ Scala/SimpleBitCoinMiner/src/main/scala/Miner.scala
@@ -1,21 +1,20 @@
 import akka.actor.Actor
 import akka.actor.ActorLogging
-import scala.util.Random
 import com.roundeights.hasher.Implicits._
- 
+
 class Miner extends Actor with ActorLogging{
 
     def receive = {
-        case Job(baseStr, start, end, prefix) =>  
+        case Job(baseStr, start, end, prefix) =>
             log.info(s"Get job: start $start, end $end")
             var sha256 = baseStr.sha256
-            var randomText = ""
+            var text = ""
             var i = start
             while (i < end) {
-                randomText = baseStr ++ i.toString(36)
-                sha256 = randomText.sha256
+                text = baseStr ++ i.toString(36)
+                sha256 = text.sha256
                 if (sha256.hex.startsWith(prefix)) {
-                    sender ! Result(randomText +"\t" + sha256.hex)
+                    sender ! Result(text +"\t" + sha256.hex)
                 }
                 i = i + 1
             }
